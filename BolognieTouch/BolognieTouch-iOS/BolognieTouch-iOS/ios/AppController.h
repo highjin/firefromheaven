41a6e3be4c78594ae1fbd5abb6fbbc22db6055d6
Wander <lywander+reg@gmail.com>
2011-09-20 16:32:13 -0700

    Add BolognieTouch-iOS project

---
+++ BolognieTouch/BolognieTouch-iOS/BolognieTouch-iOS/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  BolognieTouch_iOSAppController.h
+//  BolognieTouch-iOS
+//
+//  Created by  on 11-9-20.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController	*viewController;
+}
+
+@end
+
---
+++ BolognieTouch/BolognieTouch-iOS/BolognieTouch-iOS/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BolognieTouch-iOS
+//
+//  Created by  on 11-9-20.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
