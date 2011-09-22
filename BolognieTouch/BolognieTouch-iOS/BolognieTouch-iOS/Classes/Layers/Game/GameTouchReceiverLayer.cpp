a8f9920b6b93a405059d58af98f64c1132702ce6
Wander <lywander+reg@gmail.com>
2011-09-21 20:24:24 -0700

    背景图切换，音乐循环，收工

---
+++ BolognieTouch/BolognieTouch-iOS/BolognieTouch-iOS/Classes/Layers/Game/GameTouchReceiverLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  GameTouchReceiverLayer.cpp
+//  BolognieTouch-iOS
+//
+//  Created by  on 11-9-21.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ BolognieTouch/BolognieTouch-iOS/BolognieTouch-iOS/Classes/Layers/Game/GameTouchReceiverLayer.h
@@ -0,0 +1,14 @@
+//
+//  GameTouchReceiverLayer.h
+//  BolognieTouch-iOS
+//
+//  Created by  on 11-9-21.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef BolognieTouch_iOS_GameTouchReceiverLayer_h
+#define BolognieTouch_iOS_GameTouchReceiverLayer_h
+
+
+
+#endif
